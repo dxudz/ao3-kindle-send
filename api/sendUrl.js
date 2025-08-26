d83d99126d094863c1963708c4892b26af991a33
Maria F <dxuds13@gmail.com>
2025-08-26 13:36:34 -0300

    changed cors

--- api/sendUrl.js
+++ api/sendUrl.js
@@ -2,14 +2,14 @@ import fetch from "node-fetch";
 import nodemailer from "nodemailer";
 
 export default async function handler(req, res) {
-  // Allow CORS for any origin (you can restrict this to AO3 later)
+  // Always set CORS headers
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
-  // Handle OPTIONS preflight request
+  // Preflight request
   if (req.method === "OPTIONS") {
-    return res.status(200).end();
+    return res.status(204).end(); // No content for OPTIONS
   }
 
   if (req.method !== "POST") {
