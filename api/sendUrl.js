f64968b48f52f716f303d5fe09e5cb8153719a3f
Maria F <dxuds13@gmail.com>
2025-08-26 13:38:51 -0300

    ao3 cors

--- api/sendUrl.js
+++ api/sendUrl.js
@@ -3,7 +3,7 @@ import nodemailer from "nodemailer";
 
 export default async function handler(req, res) {
   // Always set CORS headers
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "https://archiveofourown.org");
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
